de9a58aca4a1594979d490dbf3a24ef0893a6c5c
Eduardo Rolim <vndmtrx@gmail.com>
2018-10-09 20:56:17 -0300

    Adicionado processamento dos metadados

--- flask_staticsite/page.py
+++ flask_staticsite/page.py
@@ -1,36 +1,43 @@
-import yaml
+import yaml, os
 
-def _preload_header(file, encoding):
-    shield = '---\n'
+def _preload_header(file, encoding, shield='---'):
     header = {}
     try:
         with open(file) as raw:
-            r = raw.readline()
+            s = ''
+            r = raw.readline().strip()
             if r == shield:
                 while True:
                     line = raw.readline()
-                    if not line: break
-                    elif line == shield:
-                        #header.append(raw.tell() - len(shield))
+                    if not line:
+                        break
+                    elif line.strip() != shield:
+                        s += line
+                    else:
                         header['filepos'] = raw.tell()
                         break
-                    else: continue
-            raw.seek(len(shield), 0)
-            header['headers'] = raw.read(header['filepos'] - len(shield))
+            header['headers'] = yaml.safe_load(s)
     except IOError:
         print('Could not read file {0}'.format(file))
         pass
     return header
 
 class Page(object):
-    def __new__(cls, path, encoding):
-        header = _preload_header(path)
-        if len(header) == 2:
-            return super(Page, cls).__new__(cls)
+    def __new__(cls, path, encoding='utf-8', requires=()):
+        yml = _preload_header(path, encoding)
+        if len(yml) == 2:
+            obj = super(Page, cls).__new__(cls)
+            obj.meta = yml['headers']
+            obj._filepos = yml['filepos']
+            for req in requires:
+                if req not in obj.meta:
+                    raise ValueError('Required headers aren''t present')
+            return obj
         else:
             raise ValueError('File has no headers')
-
-    def __init__(self, path, encoding):
+    
+    
+    def __init__(self, path, encoding='utf-8', requires=()):
         self.path = path
         self.encoding = encoding
         self.mtime = os.path.getmtime(path)
