36ef265629fe398fe5c82996db9c5ed5d33fa3b0
Eduardo Rolim <vndmtrx@gmail.com>
2018-10-14 14:41:45 -0300

    Adicionada compatibilidade com python2, alteração do comportamento de erros.
    Foi adicionada compatilibidade inicial para python2 no código. Ainda não 
    testado.
    
    Foi removido tratamento de exceção da função preload, para que a mesma 
    possa ser capturada em níveis superiores ao invés de ser silenciada pelo 
    código.

--- flask_staticsite/page.py
+++ flask_staticsite/page.py
@@ -3,6 +3,8 @@
 
 import yaml, os
 
+from . import compatibility
+
 class PageException(Exception):
     """
     Exception crafted to be raised by Page Class and needs to be catched
@@ -11,26 +13,22 @@ class PageException(Exception):
 
 def _preload_header(file, encoding, shield='---'):
     header = {}
-    try:
-        with open(file) as raw:
-            s = ''
-            r = raw.readline().strip()
-            if r == shield:
-                while True:
-                    line = raw.readline()
-                    if not line:
-                        break
-                    elif line.strip() != shield:
-                        s += line
-                    else:
-                        header['filepos'] = raw.tell()
-                        break
-            if 'filepos' not in header:
-                raise PageException('Malformed header')
-            header['headers'] = yaml.safe_load(s)
-    except IOError:
-        print('Could not read file {0}'.format(file))
-        pass
+    with open(file, encoding=encoding) if compatibility.IS_PY3 else open(file) as raw:
+        s = ''
+        r = raw.readline().strip() if compatibility.IS_PY3 else raw.readline().decode(encoding).strip()
+        if r == shield:
+            while True:
+                line = raw.readline() if compatibility.IS_PY3 else raw.readline().decode(encoding)
+                if not line:
+                    break
+                elif line.strip() != shield:
+                    s += line
+                else:
+                    header['filepos'] = raw.tell()
+                    break
+        if 'filepos' not in header:
+            raise PageException('Malformed header in file: {0}'.format(file))
+        header['headers'] = yaml.safe_load(s)
     return header
 
 class Page(object):
@@ -43,17 +41,18 @@ class Page(object):
     
     def __new__(cls, filename, encoding='utf-8', requires=()):
         yml = _preload_header(filename, encoding)
-        if len(yml) == 2:
+        #if 'headers' in yml and 'filepos' in yml:
+        #if all(x in yml for x in ('headers','filepos')):
+        if {'headers', 'filepos'}.issubset(yml):
             obj = super(Page, cls).__new__(cls)
             obj._meta = yml['headers']
             obj._filepos = yml['filepos']
-            req = ('slug',) + requires
-            for item in req:
+            for item in requires:
                 if item not in obj._meta:
                     raise PageException('Required header is not present in file "{0}": "{1}"'.format(filename, item))
             return obj
         else:
-            raise PageException('File "{0}" has no headers'.format(filename))
+            raise PageException('No headers found in file: {0}'.format(filename))
     
     
     def __init__(self, filename, encoding='utf-8', requires=()):
@@ -74,8 +73,8 @@ class Page(object):
             return self._content
         except AttributeError:
             if self.mtime != os.path.getmtime(self.filename):
-                raise PageException('File "{0}" changed since instance creation'.format(self.filename))
-            with open(self.filename) as raw:
+                raise PageException('File changed since instance creation: {0}'.format(self.filename))
+            with open(self.filename, encoding=self.encoding) if compatibility.IS_PY3 else open(self.filename) as raw:
                 raw.seek(self._filepos, 0)
-                self._content = raw.read().strip()
+                self._content = raw.read().strip() if compatibility.IS_PY3 else raw.read().decode(encoding).strip()
             return self._content
