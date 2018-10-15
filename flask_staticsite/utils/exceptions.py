987069a4d0d8d55fbfa0757489b6254e20806cfa
Eduardo Rolim <vndmtrx@gmail.com>
2018-10-14 21:12:05 -0300

    Movimentação das classes satélites para utils.

---
+++ flask_staticsite/utils/exceptions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class SitemapException(Exception):
+    """
+    Exception crafted to be raised by Sitemap Class and needs to be catched
+    elsewhere.
+    """
+
+
+class PageException(Exception):
+    """
+    Exception crafted to be raised by Page Class and needs to be catched
+    elsewhere.
+    """
